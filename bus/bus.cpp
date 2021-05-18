9a24fb87e9e8a9b2e048c4ab8f228a86209a0061
bitglitcher <caminlobo@gmail.com>
2021-05-17 23:53:06 -0400

    Adding support for faster 16bit and 32bit operations to the bus

--- bus/bus.cpp
+++ bus/bus.cpp
@@ -17,6 +17,34 @@ void BUS::write(uint8_t data, uint32_t address)
         }
     }
 }
+void BUS::write16(uint16_t data, uint32_t address)
+{
+    //std::cout << "BUS: Write ->" << "0x" << std::hex << std::setw(8) << std::setfill('0') << address << std::endl;
+    for (auto device : devices)
+    {
+        if(device->min_address <= address && device->max_address >= address)
+        {
+            //std::cout << "Device found!" << std::endl;
+            //device->print();
+            device->write32(data, address);
+            return;
+        }
+    }
+}
+void BUS::write32(uint32_t data, uint32_t address)
+{
+    //std::cout << "BUS: Write ->" << "0x" << std::hex << std::setw(8) << std::setfill('0') << address << std::endl;
+    for (auto device : devices)
+    {
+        if(device->min_address <= address && device->max_address >= address)
+        {
+            //std::cout << "Device found!" << std::endl;
+            //device->print();
+            device->write32(data, address);
+            return;
+        }
+    }
+}
 uint8_t BUS::read(uint32_t address)
 {
     //std::cout << "BUS: Read -> " << "0x" << std::hex << std::setw(8) << std::setfill('0') << address << std::endl;
@@ -25,6 +53,32 @@ uint8_t BUS::read(uint32_t address)
         if(device->min_address <= address && device->max_address >= address)
         {
             return device->read(address);
+        }
+    }
+    return 0x0000;
+}
+
+uint16_t BUS::read16(uint32_t address)
+{
+    //std::cout << "BUS: Read -> " << "0x" << std::hex << std::setw(8) << std::setfill('0') << address << std::endl;
+    for (auto device : devices)
+    {
+        if(device->min_address <= address && device->max_address >= address)
+        {
+            return device->read16(address);
+        }
+    }
+    return 0x0000;
+}
+
+uint32_t BUS::read32(uint32_t address)
+{
+    //std::cout << "BUS: Read -> " << "0x" << std::hex << std::setw(8) << std::setfill('0') << address << std::endl;
+    for (auto device : devices)
+    {
+        if(device->min_address <= address && device->max_address >= address)
+        {
+            return device->read32(address);
         }
     }
     return 0x0000;
--- bus/bus.h
+++ bus/bus.h
@@ -10,7 +10,11 @@ class BUS
     public:
         void add(device* device);
         void write(uint8_t data, uint32_t address);
+        void write32(uint32_t data, uint32_t address);
+        void write16(uint16_t data, uint32_t address);
         uint8_t read(uint32_t address);
+        uint16_t read16(uint32_t address);
+        uint32_t read32(uint32_t address);
 
     private:
         std::vector<device*> devices;
