d4782292b7be37f063a62af8f9ddfe9d4c26892a
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-07-08 17:42:04 -0500

    add PC and IR fields

--- graphics/registers.cpp
+++ graphics/registers.cpp
@@ -26,6 +26,17 @@ registers::registers(/* args */)
         register_field->label = GTK_LABEL(gtk_label_new(register_names[i].c_str()));
         register_fields.push_back(register_field);
     }
+    //Add PC and IR
+    for(int i = 32;i < 34;i++)
+    {
+        regs_t* register_field = new regs_t;
+        register_field->entry = GTK_ENTRY(gtk_entry_new());
+        gtk_entry_set_max_width_chars(register_field->entry, strlen("0x00000000"));
+        gtk_entry_set_width_chars(register_field->entry, strlen("0x00000000"));
+        register_field->label = GTK_LABEL(gtk_label_new(register_names[i].c_str()));
+        register_fields.push_back(register_field);
+    }
+
     gtk_grid_set_column_spacing(GTK_GRID(grid), 10);
     gtk_grid_set_row_spacing(GTK_GRID(grid), 10);
     for(int i = 0; i < 8;i++)
@@ -39,6 +50,10 @@ registers::registers(/* args */)
         gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[i+24]->label), 6, i, 1, 1);
         gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[i+24]->entry), 7, i, 1, 1);
     }
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[32]->label), 0, 8, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[32]->entry), 1, 8, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[33]->label), 2, 8, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(register_fields[33]->entry), 3, 8, 1, 1);
 }
 
 registers::~registers()
